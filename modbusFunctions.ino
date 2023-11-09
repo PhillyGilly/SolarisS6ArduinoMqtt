ea932890e3efaaf5e29f7b61e312e85ddb33185b
PhillyGilly <56273663+PhillyGilly@users.noreply.github.com>
2023-11-09 13:48:43 +0000

    Update modbusFunctions.ino

--- modbusFunctions.ino
+++ modbusFunctions.ino
@@ -1,4 +1,3 @@
-
 // Address    Name             Data type Unit  divisor
 //3005-3006   activePower      U32       1W      1 
 //3009- 3010  totalEnergy      U32       1kWh    1 
@@ -13,64 +12,73 @@
 //3042        inverterTemp     U16       0.1C    10
 //3043        acFrequency      U16       0.01Hz  100
 
-void modbusData(){
+String modbusData(){
   int wait = 100;
+  unsigned long rate = 10000; // default refresh rate in ms was 60000
+  unsigned long lastMillis = 0;
  
-  if (millis() - lastMillis > rate) {
-    lastMillis = millis();
-    modbusMsg = "{";
-    //make all the read calls
-    delay(wait);
-    activePower = readactivePower();
-    delay(wait);
-    totalEnergy = readtotalEnergy();
-    delay(wait);
-    thismonthEnergy = readthismonthEnergy();
-    delay(wait);
-    lastmonthEnergy = readlastmonthEnergy();
-    delay(wait);
-    todayEnergy = readtodayEnergy();
-    delay(wait);
-    lastdayEnergy = readlastdayEnergy();
-    delay(wait);
-    thisyearEnergy = readthisyearEnergy();
-    delay(wait);
-    lastyearEnergy = readlastyearEnergy();
-    delay(wait);
-    dcVolts = readdcVolts();
-    delay(wait);
-    dcAmps = readdcAmps();
-    delay(wait);
-    inverterTemp = readinverterTemp();
-    delay(wait);
-    acFrequency = readacFrequency();
-    delay(wait);
-    modbusMsg += "}";
-    }
+  contentStr = "{\"actP\":\"";
+  contentStr += readactivePower();
+  delay(wait);
+  contentStr += "\",\"totE\":\"";
+  contentStr += readtotalEnergy();
+  delay(wait);
+  contentStr += "\",\"tmthE\":\"";
+  contentStr += readthismonthEnergy();
+  delay(wait);
+  contentStr += "\",\"lmthE\":\"";
+  contentStr += readlastmonthEnergy();
+  delay(wait);
+  contentStr += "\",\"tdyE\":\"";
+  contentStr += readtodayEnergy();
+  delay(wait);
+  contentStr += "\",\"ldyE\":\"";
+  contentStr += readlastdayEnergy();
+  delay(wait);
+  contentStr += "\",\"tyE\":\"";
+  contentStr += readthisyearEnergy();
+  delay(wait);
+  contentStr += "\",\"lyE\":\"";
+  contentStr += readlastyearEnergy();
+  delay(wait);
+  contentStr += "\",\"dcV\":\"";
+  contentStr += readdcVolts();
+  delay(wait);
+  contentStr += "\",\"dcA\":\"";
+  contentStr += readdcAmps();
+  delay(wait);
+  contentStr += "\",\"invT\":\"";
+  contentStr += readinverterTemp();
+  delay(wait);
+  contentStr += "\",\"acHz\":\"";
+  contentStr += readacFrequency();
+  delay(wait);
+  contentStr += "\"}";
+  return contentStr;
 }
 
 double readactivePower(){
-  activePower = 0;
-  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3004, 2)) {
-    modbusMsg += "\"activePower\":\"";
-    modbusMsg += ModbusRTUClient.lastError();
-    modbusMsg += "\",";
-  }else{
+  if(!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3004, 2))
+  {
+    activePower = 0;
+  }
+  else
+  {
     uint16_t word1 = ModbusRTUClient.read();
     uint16_t word2 = ModbusRTUClient.read();
     int32_t double1 = word1 << 16 | word2;
     activePower = double1;
+    return activePower;
   }
-  return activePower;
 }  
 
 double readtotalEnergy(){
-  totalEnergy = 0;
-  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3008, 2)) {
-    modbusMsg += "\"totalEnergy\":\"";
-    modbusMsg += ModbusRTUClient.lastError();
-    modbusMsg += "\",";
-  }else{
+  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3008, 2))
+  {
+    totalEnergy = totalEnergy;
+  }
+  else
+  {
     uint16_t word1 = ModbusRTUClient.read();
     uint16_t word2 = ModbusRTUClient.read();
     int32_t double1 = word1 << 16 | word2;
@@ -80,12 +88,12 @@ double readtotalEnergy(){
 }  
 
 double readthismonthEnergy(){
-  thismonthEnergy = 0;
-  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3010, 2)) {
-    modbusMsg += "\"thismonthEnergy\":\"";
-    modbusMsg += ModbusRTUClient.lastError();
-    modbusMsg += "\",";
-  }else{
+  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3010, 2))
+  {
+    thismonthEnergy = thismonthEnergy;
+  }
+  else
+  {
     uint16_t word1 = ModbusRTUClient.read();
     uint16_t word2 = ModbusRTUClient.read();
     int32_t double1 = word1 << 16 | word2;
@@ -95,11 +103,12 @@ double readthismonthEnergy(){
 }
 
 double readlastmonthEnergy(){
-  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3012, 2)) {
-    modbusMsg += "\"lastmonthEnergy\":\"";
-    modbusMsg += ModbusRTUClient.lastError();
-    modbusMsg += "\",";
-  }else{
+  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3012, 2))
+  {
+    lastmonthEnergy=lastmonthEnergy;
+  }
+  else
+  {
     uint16_t word1 = ModbusRTUClient.read();
     uint16_t word2 = ModbusRTUClient.read();
     int32_t double1 = word1 << 16 | word2;
@@ -109,11 +118,12 @@ double readlastmonthEnergy(){
 }
 
 float readtodayEnergy(){
-  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3014, 1)) {
-    modbusMsg += "\"todayEnergy\":\"";
-    modbusMsg += ModbusRTUClient.lastError();
-    modbusMsg += "\",";
-  }else{
+  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3014, 1))
+  {
+    todayEnergy=todayEnergy;
+  }
+  else
+  {
     uint16_t word1 = ModbusRTUClient.read();  
     todayEnergy = word1/10;
   }
@@ -121,11 +131,12 @@ float readtodayEnergy(){
 }
 
 float readlastdayEnergy(){
-  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3015, 1)) { 
-    modbusMsg += "\"lastdayEnergy\":\"";
-    modbusMsg += ModbusRTUClient.lastError();
-    modbusMsg += "\",";
-  }else{
+  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3015, 1))
+  {
+    lastdayEnergy = lastdayEnergy;
+  }
+  else
+  {
     uint16_t word1 = ModbusRTUClient.read();  
     lastdayEnergy = word1/10;
   }
@@ -133,11 +144,12 @@ float readlastdayEnergy(){
 }
 
 double readthisyearEnergy(){
-  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3016, 2)) {
-    modbusMsg += "\"thisyearEnergy\":\"";
-    modbusMsg += ModbusRTUClient.lastError();
-    modbusMsg += "\",";
-  }else{
+  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3016, 2))
+  {
+    thisyearEnergy = thisyearEnergy;
+  }
+  else
+  {
     uint16_t word1 = ModbusRTUClient.read();
     uint16_t word2 = ModbusRTUClient.read();
     int32_t double1 = word1 << 16 | word2;
@@ -147,25 +159,27 @@ double readthisyearEnergy(){
 }
 
 double readlastyearEnergy(){
-  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3018, 2)) {
-    modbusMsg += "\"lastyearEnergy\":\"";
-    modbusMsg += ModbusRTUClient.lastError();
-    modbusMsg += "\",";
-  }else{
+  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3018, 2))
+  {
+    lastyearEnergy = lastyearEnergy;
+  }
+  else
+  {
     uint16_t word1 = ModbusRTUClient.read();
     uint16_t word2 = ModbusRTUClient.read();
     int32_t double1 = word1 << 16 | word2;
     lastyearEnergy = double1;
   }
-  return thisyearEnergy;
+  return lastyearEnergy;
 }
 
 float readdcVolts(){
-  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3021, 1)) { 
-    modbusMsg += "\"dcVolts\":\"";
-    modbusMsg += ModbusRTUClient.lastError();
-    modbusMsg += "\",";
-  }else{
+  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3021, 1))
+  {
+    dcVolts = 0;
+  }
+  else
+  {
     uint16_t word1 = ModbusRTUClient.read();  
     dcVolts = word1/10;
   }
@@ -173,11 +187,12 @@ float readdcVolts(){
 }
 
 float readdcAmps(){
-  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3022, 1)) { 
-    modbusMsg += "\"dc Amps\":\"";
-    modbusMsg += ModbusRTUClient.lastError();
-    modbusMsg += "\",";
-  }else{
+  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3022, 1))
+  {
+    dcAmps = 0;
+  }
+  else
+  {
     uint16_t word1 = ModbusRTUClient.read();  
     dcAmps = word1/10;
   }
@@ -185,11 +200,12 @@ float readdcAmps(){
 }
 
 float readinverterTemp(){
-  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3041, 1)) { 
-    modbusMsg += "\"inverterTemp\":\"";
-    modbusMsg += ModbusRTUClient.lastError();
-    modbusMsg += "\",";
-  }else{
+  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3041, 1))
+  {
+    inverterTemp = inverterTemp;
+  }
+  else
+  {
     uint16_t word1 = ModbusRTUClient.read();  
     inverterTemp = word1/10;
   }
@@ -197,13 +213,53 @@ float readinverterTemp(){
 }
 
 float readacFrequency(){
-  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3042, 1)) { 
-    modbusMsg += "\"acFrequency\":\"";
-    modbusMsg += ModbusRTUClient.lastError();
-    modbusMsg += "\"";
-  }else{
+  if (!ModbusRTUClient.requestFrom(1, INPUT_REGISTERS, 3042, 1))
+  {
+    acFrequency = acFrequency;
+  }
+  else
+  {
     uint16_t word1 = ModbusRTUClient.read();  
     acFrequency = word1/100;
   }
   return acFrequency;
 }
+
+// Address    Name             Data type
+//3000        Year             U16       
+//3001        Month            U16 
+//3002        Day              U16 
+//3003        Hours            U16 
+//3004        Minutes          U16 
+//3005        Seconds          U16 
+String modbusTime(){
+  String contentStr;
+  if (!ModbusRTUClient.requestFrom(1, HOLDING_REGISTERS, 2999, 6))
+  { 
+    contentStr += "\"Time Error\":\"";
+    contentStr += ModbusRTUClient.lastError();
+    contentStr += "\"";
+  }
+  else
+  {
+    contentStr = "{\"invYr\":\"";
+    uint16_t word1 = ModbusRTUClient.read();  
+    contentStr += word1;
+    contentStr += "\",\"invMth\":\"";
+    uint16_t word2 = ModbusRTUClient.read();  
+    contentStr += word2;
+    contentStr += "\",\"invDay\":\"";
+    uint16_t word3 = ModbusRTUClient.read();  
+    contentStr += word3;
+    contentStr += "\",\"invHr\":\"";
+    uint16_t word4 = ModbusRTUClient.read();  
+    contentStr += word4;
+    contentStr += "\",\"invMin\":\"";
+    uint16_t word5 = ModbusRTUClient.read();  
+    contentStr += word5;
+    contentStr += "\",\"invSec\":\"";
+    uint16_t word6 = ModbusRTUClient.read();  
+    contentStr += word6;
+    contentStr += "\"}";
+  }
+  return contentStr;
